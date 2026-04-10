1d85129ff8e98260675ee5e5b62acfa716a9f682
Pranaya142 <kommerapranaya@gmail.com>
2026-04-10 16:50:19 +0530

    Add files via upload

---
+++ Opencv/Haarscade/Face_detection.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+# call casecade classifieers
+face_class = cv2.CascadeClassifier(r'C:\Pranaya\AI\Deep_learning\opencv\haarcascade_classifier\face_default.xml')
+
+# check whether the file is available or not
+if face_class.empty():
+    print("not file founded")
+    exit()
+image = cv2.imread(r'C:\Pranaya\AI\images\samvedya_ai.png')
+
+#check the image loded or not
+'''if not image.isOpen():
+    print("no image is found")
+    exit()'''
+# convert the image to gray scale
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+# detect the faces in the image
+face = face_class.detectMultiScale(gray,1.3,5)
+
+# draw rectangle around the faces
+for (x,y,w,h) in face:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(127,0,255),2)
+# show the output image
+cv2.imshow("face detection",image)
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ Opencv/Haarscade/eye_detection.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+# call casecade classifieers
+face_class = cv2.CascadeClassifier(r'C:\Pranaya\AI\Deep_learning\opencv\haarcascade_classifier\face_default.xml')
+eye_class = cv2.CascadeClassifier(r'C:\Pranaya\AI\Deep_learning\opencv\haarcascade_classifier\eye.xml')
+# check whether the file is available or not
+if face_class.empty():
+    print("not file founded")
+    exit()
+if eye_class.empty():
+    print("not file founded")
+    exit()
+image = cv2.imread(r'C:\Pranaya\AI\images\samvedya_ai.png')
+
+#check the image loded or not
+'''if not image.isOpen():
+    print("no image is found")
+    exit()'''
+# convert the image to gray scale
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+# detect the faces in the image
+face = face_class.detectMultiScale(gray,1.3,5)
+eye = eye_class.detectMultiScale(gray,1.3,5)
+
+# draw rectangle around the faces
+for (x,y,w,h) in face:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(127,0,255),2)
+    for (ex,ey,ew,eh) in eye:
+        cv2.rectangle(image,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+# show the output image
+cv2.imshow("face detection",image)
+cv2.waitKey()
+cv2.destroyAllWindows()
